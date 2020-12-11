b0b974622d9f38cf6408f9522e0d1e390f0d9597
jaana.seffer@khk.ee <jaana.seffer@khk.ee>
2020-12-11 16:35:41 +0200

    deploy dockeri jaoks

--- techworld/app/server.js
+++ techworld/app/server.js
@@ -70,6 +70,6 @@ app.get('/get-profile', function (req, res) {
   });
 });
 
-app.listen(3000, function () {
-  console.log("app listening on port 3000!");
+app.listen(3001, function () {
+  console.log("app listening on port 3001!");
 });
